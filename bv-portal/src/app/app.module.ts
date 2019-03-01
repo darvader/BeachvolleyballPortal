040d5b6ac91d40e2a05a536881313dd618554037
afiedler <A.Fiedler@intershop.de>
2019-03-01 17:13:04 +0100

    Added tournament components

--- bv-portal/src/app/app.module.ts
+++ bv-portal/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TournamentListComponent } from './tournament/tournament-list/tournament-list.component';
+import { TournamentListItemComponent } from './tournament/tournament-list-item/tournament-list-item.component';
+import { TournamentDetailComponent } from './tournament/tournament-detail/tournament-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TournamentListComponent,
+    TournamentListItemComponent,
+    TournamentDetailComponent
   ],
   imports: [
     BrowserModule,
