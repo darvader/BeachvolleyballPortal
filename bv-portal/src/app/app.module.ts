546abf7fb3236c1746726d2529d4163b1b1acdae
Andreas Fiedler <fiedler.andi@gmail.com>
2019-03-08 18:00:32 +0100

    Removed not needed list item.

--- bv-portal/src/app/app.module.ts
+++ bv-portal/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule, APP_INITIALIZER, Provider, LOCALE_ID } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TournamentListComponent } from './tournament/tournament-list/tournament-list.component';
-import { TournamentListItemComponent } from './tournament/tournament-list-item/tournament-list-item.component';
 import { TournamentDetailComponent } from './tournament/tournament-detail/tournament-detail.component';
 import { SharedModule, INIT_API_CONFIGURATION } from './shared/shared.module';
 import { MatTableModule} from '@angular/material/table';
@@ -23,7 +22,6 @@ import { PlayerListComponent } from './player-list/player-list.component';
   declarations: [
     AppComponent,
     TournamentListComponent,
-    TournamentListItemComponent,
     TournamentDetailComponent,
     EditTournamentComponent,
     PlayerComponent,
